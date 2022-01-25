6dbfe908f67c77f05d6fe491f687ef53aab2f549
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-01-25 20:03:37 +0530

    Add problemList view

--- problem/views.py
+++ problem/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Problem
+from .serializers import ProblemSerializer
 
 # Create your views here.
+@api_view(['GET'])
+def problemList(request):
+    problems = Problem.objects.all()
+    print(problems)
+    serializer = ProblemSerializer(problems, many=True)
+    print(serializer)
+    return Response(serializer.data)
