fb677531f5d3f166562a47d479e84afd97f7bfa9
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-01-27 11:44:58 +0530

    connected tag and problem serializer

--- problem/serializers.py
+++ problem/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import Problem, Tag
 
-class ProblemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Problem
-        fields = '__all__'
-
-
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
+        fields = '__all__'
+
+
+class ProblemSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+    class Meta:
+        model = Problem
         fields = '__all__'
