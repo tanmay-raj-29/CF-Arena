c55eee9b5f4aaf9f5f8da05ee334e973169f8a63
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-01-27 11:43:49 +0530

    bug fix - replaced __name__ with __str__

--- problem/models.py
+++ problem/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(default='tag', unique=True, max_length=50, blank=False)
 
-    def __name__(self):
+    def __str__(self):
         return str(self.name)
 
 
