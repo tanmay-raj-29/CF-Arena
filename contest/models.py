87f74f7509a2cedfb41674d9c384566f83f4ccc7
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-03-27 22:47:06 +0530

    Add contest model

--- contest/models.py
+++ contest/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contest(models.Model):
-    contest_id = models.UUIDField(blank=False, primary_key=True, db_index=True)
+    contest_id = models.IntegerField(blank=False, primary_key=True, db_index=True)
     contest_duration = models.IntegerField(blank=True, default=120)
     name = models.CharField(max_length=250, default='Codeforces Round #0')
     
