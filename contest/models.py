ecf87fa80c0b40df9a771d36e1341714d87c945b
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-03-27 19:25:57 +0530

    Change interger to UUID field

--- contest/models.py
+++ contest/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Contest(models.Model):
-    contest_id = models.IntegerField(blank=False, primary_key=True)
+    contest_id = models.UUIDField(blank=False, primary_key=True, db_index=True)
     contest_duration = models.IntegerField(blank=True, default=120)
     name = models.CharField(max_length=250, default='Codeforces Round #0')
-
+    
     def __str__(self):
         return self.name
