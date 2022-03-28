a4cdd29de54b775d2a3afadc0960b542e6b4975c
aadharkansal <aadharkansal.cer18@iitbhu.ac.in>
2022-03-29 02:41:32 +0530

    added filterset backend

--- CF_Arena/settings.py
+++ CF_Arena/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'django_filters',
     # local apps
     'user',
     'problem',
@@ -106,6 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
